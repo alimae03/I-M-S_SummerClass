8e6500421023b9420cd2bd1b14ea9b0e27cb4f92
alimae03 <alisinforoso2004@gmail.com>
2026-07-08 16:14:28 +0800

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 const express = require("express");
 const app = express();
-const port = "3000";
-const dotenv = require("dotenv").config();
+const port = 3000;
+
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 const connectionString = process.env.CONNECT_STRING;
-const {add, subtract} = require("./helpers/math"); 
+
+const { add, subtract } = require("./helpers/math");
 console.log(subtract(90, 12));
 
+const { result } = require("./helpers/loggers/result");
+console.log(result());
+
 app.get("/", (req, res) => {
-	res.send("");                                                   
+    res.send("This is your Main Page");
 });
 
 app.get("/about", (req, res) => {
-	res.send('');
+    res.send("This is your About Page");
 });
 
 app.get("/contact", (req, res) => {
-	res.send('');
+    res.send("This is your Contact Page");
 });
 
 app.get("/home", (req, res) => {
-	res.send('');
+    res.send("This is your Home Page");
 });
 
-app.listen(port, ()	=> {
-	console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
-const mongoose = require("mongoose")
-	.connect(connectionString)
-	.then(() => console.log("Connected to MongoDB"))
-	.catch((err) => console.log(err));
+mongoose
+    .connect(connectionString)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
